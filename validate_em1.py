d9b7f35f12e8ec61b51b4564a15b867ba7f0b822
Richard Bowen <richard-bowen@live.co.uk>
2015-09-01 20:27:41 +0100

    first commit

---
+++ validate_em1.py
@@ -0,0 +1,25 @@
+from email_validator import validate_email, EmailNotValidError
+
+import os
+import pandas
+
+
+
+os.chdir(r'c:/users/r.bowen/desktop')
+
+emaildf = pandas.read_csv('Emails_check.csv',low_memory=False)
+
+def email_valid(email):
+
+    try:
+        v = validate_email(email) # validate and get info
+        email_v = v["email"] # replace with normalized form
+        return (email_v, 'Success')
+    except EmailNotValidError as e:
+        # email is not valid, exception message is human-readable
+        return (email, e)
+    
+
+    
+emaildf['Validate'] = emaildf['Email'].apply(email_valid)
+
